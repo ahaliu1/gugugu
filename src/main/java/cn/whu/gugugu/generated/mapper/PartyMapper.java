fa938981a3bf14a9c533c8a3092e1ad3870a7542
ahaliu1 <247969917@qq.com>
2019-04-18 22:46:02 +0800

    添加@Repository标记

--- src/main/java/cn/whu/gugugu/generated/mapper/PartyMapper.java
+++ src/main/java/cn/whu/gugugu/generated/mapper/PartyMapper.java
@@ -4,7 +4,9 @@ import cn.whu.gugugu.generated.model.Party;
 import cn.whu.gugugu.generated.model.PartyExample;
 import java.util.List;
 import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PartyMapper {
     long countByExample(PartyExample example);
 
